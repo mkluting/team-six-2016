d0f06c75a7d81b770a588c3fe98a3a5740f6d91e
Jeff Zotz <zotzjm@gmail.com>
2016-01-30 19:00:37 +0000

    Added delete and post to destinations.js 	modified:   destinations.js

--- routes/destinations.js
+++ routes/destinations.js
@@ -2,29 +2,52 @@ var express = require('express');
 var router = express.Router()
 var connection = require('../config/db');
 
+//contributors: jeff zotz
+
 
 router.use(function(req, res, next) {
 	//This will be run before any endpoint function
 	next();
 });
 
-
+//get all destinations
 router.get('/', function (req, res) {
 	//res.json({message: 'hello world'});
 	connection.query('SELECT * FROM Destinations;', function (err, rows, fields) {
-        if (err) throw err; 
-        res.json(rows);
+        	if (err) throw err; 
+        	res.json(rows);
 	});
 });
 
-
+//get single destination
 router.get('/:id', function (req, res) {
 	//res.json({message: 'hello world'});
 	var datId = req.params.id;
 	connection.query('SELECT * FROM Destinations WHERE id = '+ datId +';', function (err, rows, fields) {
-        if (err) throw err; 
-        res.json(rows);
+        	if (err) throw err; 
+        	res.json(rows);
 	});
 });
+
+// create
+router.post('/', function (req, res) {
+        connection.query('INSERT INTO Destinations SET ?', req.body, function(err, result) {
+                if (err) throw err;
+                var returnObject = {'created':true};
+                res.json(returnObject);
+        });
+});
+
+// delete
+router.delete('/:id', function (req, res) {
+	var datId = req.params.id;
+	connection.query('DELETE FROM Destinations WHERE id = '+ datId +';', function (err, rows, fields) {
+        	if (err) throw err;
+		var returnObject = {'deleted':true};
+        	res.json(returnObject);
+        });
+
+});
+
 
 module.exports = router;
