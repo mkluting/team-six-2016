8b1719b1c68b66f6420a5aba9b3db02a706f05d5
Jono Lane <jonolane@pop.belmont.edu>
2016-01-30 17:43:04 +0000

    Added get functions to Phases

--- routes/phases.js
+++ routes/phases.js
@@ -9,10 +9,29 @@ router.use(function(req, res, next) {
 	next();
 });
 
+var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password1',
+        database: 'voyage'
+});
+connection.connect();
+
 router.get('/', function (req, res) {
-	res.json({message: 'hello world'});
+        //res.json({message: 'hello world'});
+        connection.query('SELECT * FROM Phases;', function (err, rows, fields) {
+        if (err) throw err;
+        res.json(rows);
+        });
+});
+
+router.get('/:id', function (req, res) {
+        //res.json({message: 'hello world'});
+        var datId = req.params.id;
+        connection.query('SELECT * FROM Phases  WHERE id = '+ datId +';', function (err, rows, fields) {
+        if (err) throw err;
+        res.json(rows);
+        });
 });
 
 module.exports = router;
-
-//This is where I'm going to put the git single phase api call
