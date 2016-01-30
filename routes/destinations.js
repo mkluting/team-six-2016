6e98fff22609f745f8ab48adda7f44a14629e181
Jeff Zotz <zotzjm@gmail.com>
2016-01-30 17:32:29 +0000

    Added the single id get for destinations.js 	modified:   destinations.js

--- routes/destinations.js
+++ routes/destinations.js
@@ -2,13 +2,36 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
+
 router.use(function(req, res, next) {
 	//This will be run before any endpoint function
 	next();
 });
 
+var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password1',
+        database: 'voyage'
+});
+connection.connect();
+
 router.get('/', function (req, res) {
-	res.json({message: 'hello world'});
+	//res.json({message: 'hello world'});
+	connection.query('SELECT * FROM Destinations;', function (err, rows, fields) {
+        if (err) throw err; 
+        res.json(rows);
+	});
+});
+
+
+router.get('/:id', function (req, res) {
+	//res.json({message: 'hello world'});
+	var datId = req.params.id;
+	connection.query('SELECT * FROM Destinations WHERE id = '+ datId +';', function (err, rows, fields) {
+        if (err) throw err; 
+        res.json(rows);
+	});
 });
 
 module.exports = router;
