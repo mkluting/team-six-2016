fa8e353d9bf966d22cd1742e136a2a8e51011ec6
reeceboyd <reece.boyd@mtsu.edu>
2016-01-30 17:55:16 +0000

    Added functionality for getting a single attraction, getting all attractions at given destination, and adding an attraction

--- routes/attractions.js
+++ routes/attractions.js
@@ -7,7 +7,51 @@ router.use(function(req, res, next) {
 	next();
 });
 
+//Configure the MySQL connection
+var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password1',
+        database: 'voyage'
+});
+
+// read
+router.get('/:id', function (req, res) {
+	
+	var attractionId = req.params.id;
+	connection.query('select * from Attractions where id = ' + attractionId + ';', function(err, rows, fields) {
+	if (err) throw err;
+	res.json(rows);	
+	});
+});
+
 router.get('/', function (req, res) {
+	var destId = req.query.destination_id;
+	if (destId) {
+	connection.query('select * from Attractions where dest_id = ' + destId + ';', function(err, rows, fields) {
+	if (err) throw err;
+	res.json(rows);	
+	});
+}
+});
+
+// create
+router.post('/', function (req, res) {
+	connection.query('INSERT INTO Attractions SET ?', req.body, function(err, result) {
+		if (err) throw err;
+		var returnObject = {'created':true}
+		res.json(returnObject);	
+	});
+});
+
+
+// delete
+router.delete('/', function (req, res) {
+	res.json({message: 'hello world'});
+});
+
+// update
+router.put('/', function (req, res) {
 	res.json({message: 'hello world'});
 });
 
