e5ab3bf4d572768d3c02b3263bd6fdf269b67cb5
Matt Kluting <matt@ip-172-31-22-67.ec2.internal>
2016-01-31 05:06:49 +0000

    Got index pretty far

--- static/js/index.js
+++ static/js/index.js
@@ -8,7 +8,8 @@ $(document).ready(function() {
 		},
 
 		config : {
-			getUrl : '/api/phases'
+			getUrl : '/api/phases',
+			updateSortUrl : '/api/phases/phase_sort'
 		},
 
 		// initialize functionality
@@ -20,6 +21,11 @@ $(document).ready(function() {
 
 		initClickEvents : function () {
 			var self = this;
+
+			// on click of add leg
+			$('button.new-leg').on('click', function() {
+				alert('if only there was time..');	
+			});	
 		},
 
 		getPhases : function() {
@@ -35,7 +41,6 @@ $(document).ready(function() {
 
 			xhr.success(function(data) {
 				var phases = data;
-				console.log(phases);
 
 				if (phases.length <= 0){
 					//no phases 
@@ -45,10 +50,10 @@ $(document).ready(function() {
 				$.each(phases, function(index, val) {
 					var phase = val;
 
-					var	$leg = $('<li class="leg panel panel-default" data-id="'+phase.id+'"></li>'),
-						$legHeader = $('<div class="leg-header panel-heading col-md-12"><div class="header col-md-9">Leg <span class="leg-number">'+count+'</span> - '+phase.name+'</div></div>'),
+					var	$leg = $('<li class="leg panel panel-default col-md-12" data-id="'+phase.id+'"></li>'),
+						$legHeader = $('<div class="leg-header panel-heading col-md-12"><div class="header col-md-9">Leg <span class="leg-number">'+count+'</span> - <span class="leg-name">'+phase.name+'</span></div></div>'),
 						$actions = $('<div class="leg-header-actions col-pull-right col-md-3"></div>'),
-						$addDestination = $('<button class="btn btn-primary add-destionation">Add Destination</button>'),
+						$addDestination = $('<button class="btn btn-primary add-destination">Add Destination</button>'),
 						$deleteLeg = $('<button class="btn btn-warning delete-leg">X</div>'),
 						$legBody = $('<div class="leg-body panel-body"></div>');
 
@@ -56,6 +61,22 @@ $(document).ready(function() {
 					$actions.append($deleteLeg);
 					$legHeader.append($actions);
 					$leg.append($legHeader);
+
+					$.each(phase.destinations, function(index, value){
+						var destination = value;
+
+						var $dest = $('<div class="destination col-md-12"></div>'),
+							$photo = $('<div class="photo col-md-2"><img src="'+destination.dest_photo+'" height="75px" width="75px"/></div>'),
+							$header = $('<div class="destination-header col-md-6">'+destination.name+'</div>'),
+							$distance = $('<div class="distance col-md-2">'+destination.distance+' Miles</div>'),
+							$mapPhoto = $('<div class="map-photo col-md-2"><img height="75px" width="75px" src="'+destination.map_photo+'"/></div>');
+
+						$dest.append($photo);
+						$dest.append($header);
+						$dest.append($distance);
+						$dest.append($mapPhoto);							
+						$legBody.append($dest); 
+					});
 					$leg.append($legBody);
 					
 					self.cache.legsListing.append($leg);
@@ -63,6 +84,7 @@ $(document).ready(function() {
 				});
 
 				self.cache.legsListing.sortable({
+					handle: $('.leg-header'),
 					start: function(event, ui) {
 						var start_pos = ui.item.index();
 				        ui.item.data('start_pos', start_pos);
@@ -75,19 +97,38 @@ $(document).ready(function() {
 							$(this).find('.leg-number').html(index+1);
 						});
 
-						if($('.update-order-btn').length <= 0) {
-							console.log('adding button');
-						
-							var $btn = $('<button class="update-order-btn btn-danger">Update Order</button>');
+						if($('.update-order-btn').length <= 0 && start_pos != new_pos) {
+							var $btn = $('<button class="update-order-btn btn-danger">Update</button>');
 							$('.voyages-body').prepend($btn);	
 
 							//click event for updating
 							$btn.on('click', function() {
-								alert('updating sort');
 								self.updateSort();
 							});	
 						}
 					}
+				});
+
+/*
+				$('.leg-name').dblClick(function(e) {
+					$(this).html('<input class="thVal" type="text" value="' + value + '" />');
+				    $(".thVal").focus();
+				    $(".thVal").keyup(function (event) {
+			        	if (event.keyCode == 13) {
+            				$(currentEle).html($(".thVal").val().trim());
+        				}
+    				});
+
+    				$(document).click(function () {
+            			$(currentEle).html($(".thVal").val().trim());
+				    });	
+				});*/
+
+				$('button.add-destination').on('click', function() {
+					alert('if only there was time..');
+				});
+				$('button.delete-leg').on('click', function() {
+					alert('if only there was time...');
 				});
 
 				self.cache.loading.fadeOut(1000, function() {
@@ -103,14 +144,26 @@ $(document).ready(function() {
 		},
 
 		updateSort : function() {
+			var self = this;
 			var data = {};
 			
 			$.each($('.legs-listing').children('.leg'), function(index, val) {
 				var id = $(this).attr('data-id');
-				data.id = index+1;
+				data[id.toString()] = (index+1).toString();
 			});
+
 			console.log(data);
-			return false;
+			var xhr = $.ajax({
+				url: self.config.updateSortUrl,
+				data: data,
+				type: 'POST',	
+			});
+			xhr.always(function(data){
+			});
+			xhr.success(function(data) {
+				window.location = '/';	
+				return false;
+			});
 		}
 	};
 
